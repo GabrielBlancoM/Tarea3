04658a29272c39c2021694a823a549302669e55a
GabrielBlancoM <gaboblanco25@icloud.com>
2020-10-14 21:49:41 -0600

    setup.py modificado

--- Package/setup.py
+++ Package/setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup
 
+setup(
+    name="Package", # Replace with your own username
+    version="0.0.1",
+    author="GabrielBlancoMora",
+    author_email="gaboblanco256@gmail.com",
+    description="A small ecample package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="git@github.com:GabrielBlancoM/Tarea3.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6
+     
+)
